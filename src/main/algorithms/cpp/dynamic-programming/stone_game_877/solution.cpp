eece07f1bf5f0e8a486f9681a9528dcd1f5ca43e
gaoyuanchong <code_xxgao@163.com>
2019-03-12 23:38:44 +0800

    add solution and explain to 877 copy solution from lee215, and the url is https://leetcode.com/problems/stone-game/discuss/154610/C%2B%2BJavaPython-DP-or-Just-return-true

---
+++ src/main/algorithms/cpp/dynamic-programming/stone_game_877/solution.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        // copying method from https://leetcode.com/problems/stone-game/discuss/154610/C%2B%2BJavaPython-DP-or-Just-return-true
+        const int nSize = piles.size();
+        if (nSize == 0) {
+            return false;
+        }
+#ifndef LIMIT_MEMORY
+        vector<vector<int> > dp(nSize, vector<int>(nSize, 0));
+        // the follow loopping means that the diff = 0;
+        for (int index = 0; index < nSize; index++) {
+            dp[index][index] = piles[index];
+        }
+        
+        for (int diff = 1; diff < nSize; diff++) {
+            for (int index = 0; index < nSize - diff; index++) {
+                dp[index][index + diff] = max(piles[index] - dp[index + 1][index + diff], 
+                                             piles[index + diff] - dp[index][index + diff - 1]);
+            }
+        }
+        return dp[0][nSize - 1];
+#elif
+        // init dp with piels, which means that the diff is zero;
+        vector<int> dp(piles);
+        for (int diff = 1; diff < nSize; diff++) {
+            for (int index = 0; index < nSize - diff; index++) {
+                // the reason why can do this is that the diff is increasing one by one.
+                // which means dp[index + 1] represents dp[index + 1][index + diff], and
+                // dp[index] represents dp[index][diff - 1]
+                // when you don't update dp[index], it saves the last exterior loop where diff can be called preDiff which is curDiff minus one;
+                dp[index] = max(piles[index] - dp[index + 1], 
+                                             piles[index + diff] - dp[index]);
+            }
+        }
+        return dp[0];
+#endif
+        
+    }
+};
