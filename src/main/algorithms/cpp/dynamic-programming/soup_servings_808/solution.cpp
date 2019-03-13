931b906572440862ef29da074e4e97cc0509dcb5
gaoxiaoxing <gaoxiaoxing@baidu.com>
2019-03-13 23:44:19 +0800

    modify multiply

--- src/main/algorithms/cpp/dynamic-programming/soup_servings_808/solution.cpp
+++ src/main/algorithms/cpp/dynamic-programming/soup_servings_808/solution.cpp
@@ -9,7 +9,6 @@ public:
     }
     double dp(int aLeft, int bLeft) {
         static const int TOTAL_DIRECTION = 4;
-        std::cout << "aLeft: " << aLeft << " bLeft: " << bLeft << std::endl;
         if (0 == (aLeft | bLeft)) {
             return 0.5;
         } else if (0 == aLeft) {
@@ -17,18 +16,12 @@ public:
         } else if (bLeft == 0) {
             return 0;
         } else {
-            double ans = 0;
-            ans += 0.25 * dp(aLeft > 100 ? aLeft - 100:0, bLeft > 0 ? bLeft : 0);
-            std::cout << "first step: " << ans << std::endl;
-            ans += 0.25 * dp(aLeft > 75 ? aLeft - 75:0, bLeft > 25 ? bLeft - 25 : 0);
-            std::cout << "second step: " << ans << std::endl;
-            ans += 0.25 * dp(aLeft > 50 ? aLeft - 50:0, bLeft > 50 ? bLeft - 50 : 0);
-            std::cout << "third step: " << ans << std::endl;
-            ans += 0.25 * dp(aLeft > 25 ? aLeft - 25:0, bLeft > 75 ? bLeft - 75: 0);
-            std::cout << "forth step: " << ans << std::endl;
-            return ans;
+            return 0.25 * (
+                dp(aLeft > 100 ? aLeft - 100:0, bLeft > 0 ? bLeft : 0) +
+                dp(aLeft > 75 ? aLeft - 75:0, bLeft > 25 ? bLeft - 25 : 0) +
+                dp(aLeft > 50 ? aLeft - 50:0, bLeft > 50 ? bLeft - 50 : 0) +
+                dp(aLeft > 25 ? aLeft - 25:0, bLeft > 75 ? bLeft - 75: 0));
         }
-       
     }
 };
 int main() {
