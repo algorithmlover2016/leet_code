fe81dd714b55840b4c66c73f5abd637ebce8ea80
gaoxiaoxing <gaoxiaoxing@baidu.com>
2019-09-12 08:56:11 +0800

    modify gen_folder to support original copying string

--- src/main/algorithms/cpp/gen_folder.sh
+++ src/main/algorithms/cpp/gen_folder.sh
@@ -1,3 +1,4 @@
-dirName=$(sed 's/ /_/g' $1 | tr '[A-Z]' '[a-z]')
+# dirName=$(sed 's/ /_/g' $1 | tr '[A-Z]' '[a-z]')
+dirName=$(sed  's/\(.*\)\(\.\) \(.*\)/\3 \1/g' $1 | sed 's/ /_/g' | tr '[A-Z]' '[a-z]')
 mkdir "./${dirName}"
 # sed 's/[A-Z]/\l&/g' $1
