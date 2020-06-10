f363a106389a00a28dbeeb403b82c78fc1e472a7
gaoxiaoxing <gaoxiaoxing@baidu.com>
2020-06-10 23:44:33 +0800

    add #include "../../head.h"

--- src/main/algorithms/cpp/gen_folder.sh
+++ src/main/algorithms/cpp/gen_folder.sh
@@ -2,4 +2,5 @@
 dirName=$(sed  's/\(.*\)\(\.\) \(.*\)/\3 \1/g' $1 | sed 's/ /_/g' | tr '[A-Z]' '[a-z]')
 mkdir "./${dirName}"
 cd "./${dirName}" && touch solution.cpp && touch description.txt && touch core.solution
+echo "#include \"../../head.h\"\n \n" > solution.cpp
 # sed 's/[A-Z]/\l&/g' $1
