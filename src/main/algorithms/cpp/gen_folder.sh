4b3d68aa56d79378b52464858915809a69519518
gaoxiaoxing <gaoxiaoxing@baidu.com>
2020-06-13 21:17:12 +0800

    modify gen_folder.sh to avoid rewrite

--- src/main/algorithms/cpp/gen_folder.sh
+++ src/main/algorithms/cpp/gen_folder.sh
@@ -1,6 +1,6 @@
 # dirName=$(sed 's/ /_/g' $1 | tr '[A-Z]' '[a-z]')
 dirName=$(sed  's/\(.*\)\(\.\) \(.*\)/\3 \1/g' $1 | sed 's/ /_/g' | tr '[A-Z]' '[a-z]')
-mkdir "./${dirName}"
+mkdir "./${dirName}" &&\
 cd "./${dirName}" && touch solution.cpp && touch description.txt && touch core.solution
-echo "#include \"../../head.h\"\n \n" > solution.cpp
+echo "#include \"../../head.h\"\n\n\n" >> solution.cpp
 # sed 's/[A-Z]/\l&/g' $1
