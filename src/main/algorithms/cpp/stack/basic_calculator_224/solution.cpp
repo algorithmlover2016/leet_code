a42c73a210c4c2e89a29c87605f2d2b1d32ba7c1
gaoxiaoxing <gaoxiaoxing@baidu.com>
2020-06-16 12:45:20 +0800

    change two stack to one, use more storage, because int ->long

--- src/main/algorithms/cpp/stack/basic_calculator_224/solution.cpp
+++ src/main/algorithms/cpp/stack/basic_calculator_224/solution.cpp
@@ -45,3 +45,41 @@ public:
 
     }
 };
+
+static int _ = []{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
+class SolutionOneStack {
+public:
+    int calculate(std::string const & s) {
+        long num=0, sign=1, result=0;
+        std::stack<long> nums;
+        for (auto &c : s) {
+            if (std::isdigit(c)) {
+                num = num*10 + (c-'0');
+            } else {
+                result += sign * num;
+                num = 0;
+                if (c == '+') {
+                    sign = 1;
+                } else if (c == '-') {
+                    sign = -1;
+                } else if (c == '(') {
+                    nums.push(result);
+                    nums.push(sign);
+                    result = 0;
+                    sign = 1;
+                } else if (c == ')') {
+                    result = nums.top() * result;
+                    nums.pop();
+                    result += nums.top();
+                    nums.pop();
+                }
+            }
+        }
+        return result + sign*num;
+    }
+};
