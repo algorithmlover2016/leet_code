e981b2930880256539761ec42aaa3a56809f34e3
gaoxiaoxing <gaoxiaoxing@baidu.com>
2020-06-17 10:26:07 +0800

    add sum_of_mutated_array_closest_to_target_1300

---
+++ src/main/algorithms/cpp/array/sum_of_mutated_array_closest_to_target_1300/solution.cpp
@@ -0,0 +1,61 @@
+#include "../../head.h"
+
+
+class Solution {
+public:
+    int findBestValue(std::vector<int> const & arr, int target) {
+        int arrSize = arr.size();
+        if (0 == arrSize) {
+            return 0;
+        }
+        // long long int sum = std::accumulate(arr.begin(), arr.end(), 0);
+        long long int sum = 0;
+        int maxEle = INT_MIN;
+        for (int index = 0; index < arrSize; index++) {
+            sum += arr[index];
+            if (arr[index] > maxEle) {
+                maxEle = arr[index];
+            }
+        }
+
+        int start = std::min(static_cast<int>(sum / arrSize), target / arrSize);
+        int end = std::min(maxEle, target);
+        int diff = INT_MAX;
+        int res = start;
+        for (; start <= end; start++) {
+            long long int tmpSum = 0;
+            // std::cout << "start: " << start << "\n";
+            for (int index = 0; index < arrSize; index++) {
+                if (arr[index] > start) {
+                    tmpSum += start;
+                } else {
+                    tmpSum += arr[index];
+                }
+            }
+            int tmpDiff = std::abs(tmpSum - target);
+            // std::cout << "tmpSum: " << tmpSum << "\t"
+            //     << "tmpDiff: " << tmpDiff <<
+            //     "\tCurDiff: " << diff << "\n";
+            if (tmpDiff < diff) {
+                diff = tmpDiff;
+                res = start;
+            } else {
+                break;
+            }
+        }
+        return res;
+
+    }
+};
+
+class Solution {
+public:
+    int findBestValue(std::vector<int>& A, int target) {
+        std::sort(A.begin(), A.end());
+        int n = A.size(), i = 0;
+        while (i < n && target > A[i] * (n - i)) {
+            target -= A[i++];
+        }
+        return i == n ? A[n - 1] : int(std::round((target - 0.0001) / (n - i)));
+    }
+};
