b627c316feb0929fae6103e9b1b308985ecf1191
gaoxiaoxing <gaoxiaoxing@baidu.com>
2020-06-20 10:07:44 +0800

    modify tree Node constructor function

--- src/main/algorithms/cpp/tree/tree.h
+++ src/main/algorithms/cpp/tree/tree.h
@@ -7,6 +7,8 @@
      int val;
      TreeNode *left;
      TreeNode *right;
-     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  };
 #endif
