a3b6a494d328edb1d29acd48e7e16e56abd3705b
gaoxiaoxing <gaoxiaoxing@baidu.com>
2020-06-21 16:33:34 +0800

    modify gen_folder.sh to avoid recreate solution.cpp when folder existed

--- src/main/algorithms/cpp/gen_folder.sh
+++ src/main/algorithms/cpp/gen_folder.sh
@@ -1,6 +1,7 @@
 # dirName=$(sed 's/ /_/g' $1 | tr '[A-Z]' '[a-z]')
 dirName=$(sed  's/\(.*\)\(\.\) \(.*\)/\3 \1/g' $1 | sed 's/ /_/g' | tr '[A-Z]' '[a-z]')
 mkdir "./${dirName}" &&\
-cd "./${dirName}" && touch solution.cpp && touch description.txt && touch core.solution
+cd "./${dirName}" &&\
+touch solution.cpp && touch description.txt && touch core.solution &&\
 echo "#include \"../../head.h\"\n\n\n" >> solution.cpp
 # sed 's/[A-Z]/\l&/g' $1
