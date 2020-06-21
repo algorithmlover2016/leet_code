2e65eb5aaf6ec7f1e055c6f844a2ef7f11f58874
gaoxiaoxing <gaoxiaoxing@baidu.com>
2020-06-21 23:23:11 +0800

    modify gen_folder.sh to fit - to _

--- src/main/algorithms/cpp/gen_folder.sh
+++ src/main/algorithms/cpp/gen_folder.sh
@@ -1,5 +1,5 @@
 # dirName=$(sed 's/ /_/g' $1 | tr '[A-Z]' '[a-z]')
-dirName=$(sed  's/\(.*\)\(\.\) \(.*\)/\3 \1/g' $1 | sed 's/ /_/g' | tr '[A-Z]' '[a-z]')
+dirName=$(sed  's/\(.*\)\(\.\) \(.*\)/\3 \1/g' $1 | sed 's/[ -]/_/g' | tr '[A-Z]' '[a-z]')
 mkdir "./${dirName}" &&\
 cd "./${dirName}" &&\
 touch solution.cpp && touch description.txt && touch core.solution &&\
