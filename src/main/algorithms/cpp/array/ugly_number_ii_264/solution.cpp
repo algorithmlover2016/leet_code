b9505dbd5b7abcfafdf8a03dfca46ef731979b64
gaoxiaoxing <gaoxiaoxing@baidu.com>
2020-06-22 10:31:54 +0800

    add ugly_number_ii_264 dp

---
+++ src/main/algorithms/cpp/array/ugly_number_ii_264/solution.cpp
@@ -0,0 +1,31 @@
+#include "../../head.h"
+
+
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        // plagiarizing from https://leetcode.com/problems/ugly-number-ii/discuss/69364/My-16ms-C%2B%2B-DP-solution-with-short-explanation
+        if (n < 4) {
+            return n;
+        }
+        std::vector<int> minMulti(n, 0);
+        minMulti[0] = 1; //n = 1, return 1
+
+        int cnt2 = 0, cnt3 = 0, cnt5 = 0;
+        for (int index = 2; index <= n; index++) {
+            minMulti[index - 1] = std::min(minMulti[cnt2] * 2, std::min(minMulti[cnt3] * 3, minMulti[cnt5]  *5));
+            // std::cout << "cnt2: " << cnt2 << "\tcnt3: " << cnt3 << "\tcnt5: " << cnt5 << "\n";
+            // std::cout << "Ugly Number: " <<minMulti[index - 1] << "\tindex: " << index << "\n";
+            if (minMulti[index - 1] == minMulti[cnt2] * 2) {
+                cnt2++;
+            }
+            if (minMulti[index - 1] == minMulti[cnt3] * 3) {
+                cnt3++;
+            }
+            if (minMulti[index - 1] == minMulti[cnt5]  *5) {
+                cnt5++;
+            }
+        }
+        return minMulti[n - 1];
+    }
+};
