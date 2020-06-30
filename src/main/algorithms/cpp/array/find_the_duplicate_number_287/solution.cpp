850baf706b7fbfd0c13083b65e85cf3a5db1e923
gaoxiaoxing <gaoxiaoxing@baidu.com>
2020-06-30 10:21:14 +0800

    add find_the_duplicate_number_287 if there is a duplicate element, there will be a ccycle in index->val

---
+++ src/main/algorithms/cpp/array/find_the_duplicate_number_287/solution.cpp
@@ -0,0 +1,40 @@
+#include "../../head.h"
+
+
+// the solution, n + 1 elements, and except the duplicate one, the other(1-n) must exist
+class Solution {
+public:
+    int findDuplicate(std::vector<int> const & nums) {
+        if (nums.empty()) {
+            return -1;
+        }
+        int res = 0;
+        for (int index = 1; index < nums.size(); index++) {
+            res ^= index ^ nums[index];
+        }
+        return res ^ nums[0];
+    }
+};
+
+class Solution {
+public:
+    int findDuplicate(std::vector<int> const & nums) {
+        if (nums.empty()) {
+            return -1;
+        }
+        int slow = nums[0], fast = nums[slow];
+        while (slow != fast) {
+            // std::cout << "\nfast, slow: " << fast << "\t" << slow << "\t";
+            fast = nums[nums[fast]];
+            slow = nums[slow];
+        }
+
+        fast = 0;
+        while (fast != slow) {
+            // std::cout << "\nfast, slow: " << fast << "\t" << slow << "\t";
+            fast = nums[fast];
+            slow = nums[slow];
+        }
+        return fast;
+    }
+};
