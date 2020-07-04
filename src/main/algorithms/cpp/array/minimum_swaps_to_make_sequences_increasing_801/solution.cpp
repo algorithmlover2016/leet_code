6591e0d4a7cb84096fa887877cef53bfdf69666d
gaoxiaoxing <gaoxiaoxing@baidu.com>
2020-07-04 10:08:21 +0800

    add minimum_swaps_to_make_sequences_increasing_801 dp solution

--- src/main/algorithms/cpp/array/minimum_swaps_to_make_sequences_increasing_801/solution.cpp
+++ src/main/algorithms/cpp/array/minimum_swaps_to_make_sequences_increasing_801/solution.cpp
@@ -37,3 +37,32 @@ public:
         return std::min(swapNum, fixNum);
     }
 };
+
+class Solution {
+public:
+    int minSwap(std::vector<int> const & A, std::vector<int> const & B) {
+        const size_t n = A.size();
+        std::vector<int> swap(n, n), no_swap(n, n);
+        swap[0] = 1;
+        no_swap[0] = 0;
+        for (size_t i = 1; i < n; ++i) {
+            if (A[i] > A[i - 1] && B[i] > B[i - 1]) {
+                // If we swap position i, we need to swap position i - 1.
+                swap[i] = swap[i - 1] + 1;
+
+                // If we don't swap position i , we should not swap position i - 1.
+                no_swap[i] = no_swap[i - 1];
+            }
+
+            if (A[i] > B[i - 1] && B[i] > A[i - 1]) {
+                // If we swap position i, we should not swap position i - 1.
+                swap[i] = std::min(swap[i], no_swap[i - 1] + 1);
+
+                // If we don't swap position i, we should swap position i - 1.
+                no_swap[i] = std::min(no_swap[i], swap[i - 1]);
+            }
+        }
+
+        return std::min(swap.back(), no_swap.back());
+    }
+};
