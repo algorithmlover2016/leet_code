8d61f6701c2b2dc28bee5c0abacbcaf7a45a7660
gaoxiaoxing <gaoxiaoxing@baidu.com>
2020-11-08 12:23:52 +0800

    add wildcard_matching_44 match reshape

---
+++ src/main/algorithms/cpp/back_tracking/wildcard_matching_44/solution.cpp
@@ -0,0 +1,69 @@
+#include "../../head.h"
+
+
+class SolutionTLE {
+public:
+    bool isMatch(std::string const & s, std::string const & p) {
+        return backTracking(s, p, 0, 0);
+    }
+    bool backTracking(std::string const & s, std::string const & p,
+            int sIndex, int pIndex) {
+        if (sIndex == s.size()) {
+            for (; pIndex < p.size(); pIndex++) {
+                if ('*' != p[pIndex]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        if (pIndex == p.size()) {
+            if ('*' != p[pIndex - 1]) {
+                return false;
+            }
+            return true;
+        }
+
+        bool ans = false;
+        if ('*' == p[pIndex]) {
+            ans = backTracking(s, p, sIndex, pIndex + 1) ||
+                backTracking(s, p, sIndex + 1, pIndex) || backTracking(s, p, sIndex + 1, pIndex + 1);
+        } else if ((s[sIndex] == p[pIndex]) || ('?' == p[pIndex])) {
+            ans = backTracking(s, p, sIndex + 1, pIndex + 1);
+        }
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    bool isMatch(std::string const & s, std::string const & p) {
+        const char *sStart = s.c_str(), *pStart = p.c_str();
+
+        const char *sRecord = nullptr;
+        const char *lastAsteriskPos = nullptr;
+        while (*sStart) {
+            if (*pStart == *sStart || '?' == *pStart) {
+                pStart++;
+                sStart++;
+                continue;
+            }
+
+            if ('*' == *pStart) {
+                lastAsteriskPos = pStart++;
+                sRecord = sStart;
+                continue;
+            }
+            if (lastAsteriskPos) {
+                sStart = ++sRecord;
+                pStart = lastAsteriskPos + 1;
+                continue;
+            }
+            return false;
+        }
+        while ('*' == *pStart) {
+            pStart++;
+        }
+        return !(*pStart); // can't using nullptr == pStart as a judgement
+    }
+
+};
