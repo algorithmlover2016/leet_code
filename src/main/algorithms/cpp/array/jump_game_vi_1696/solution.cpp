2dc8837550e70acd2cd638c1ecaf2f69e33654de
gaoxiaoxing <gaoxiaoxing@baidu.com>
2020-12-29 14:26:23 +0800

    add jump_game_vi_1696 contineous and decrease queue to record the max value in a slinding window

---
+++ src/main/algorithms/cpp/array/jump_game_vi_1696/solution.cpp
@@ -0,0 +1,35 @@
+#include "../../head.h"
+
+
+class Solution {
+public:
+    int maxResult(std::vector<int> const & nums, int k) {
+        // I can only  a idea that dp with k substructure
+        // plagiarizing from https://www.youtube.com/watch?v=dHAsYTvbbj4
+        if (nums.empty()) {
+            return 0;
+        }
+        int const numsSize = nums.size();
+
+        std::vector<int> dpMemo(numsSize, INT_MIN);
+        std::deque<int> decQue; // record the max val of dpMemo with index
+
+        dpMemo[0] = nums[0];
+        decQue.emplace_back(0);
+
+        for (int idx = 1; idx < numsSize; idx++) {
+            if (decQue.front() < idx - k) {
+                // the decQue.front() is out of sliding window
+                // sliding window from idx - k to idx
+                decQue.pop_front();
+            }
+            dpMemo[idx] = nums[idx] + dpMemo[decQue.front()];
+            while (!decQue.empty() && dpMemo[decQue.back()] <= dpMemo[idx]) {
+                decQue.pop_back();
+            }
+            decQue.emplace_back(idx);
+        }
+        return dpMemo.back();
+
+    }
+};
