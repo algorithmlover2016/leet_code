68f858f6b9ae12cb8f994cf5968490d545d8c39f
gaoxiaoxing <gaoxiaoxing@baidu.com>
2021-01-05 11:06:08 +0800

    add maximum_profit_in_job_scheduling_1235 dp problem with map, and dependent status is using upper_bound and std::prev

---
+++ src/main/algorithms/cpp/dynamic-programming/maximum_profit_in_job_scheduling_1235/solution.cpp
@@ -0,0 +1,52 @@
+#include "../../head.h"
+
+
+class Solution {
+public:
+    int jobScheduling(std::vector<int> const & startTime,
+            std::vector<int> const & endTime,
+            std::vector<int> const & profit) {
+        // plagiarizing from https://leetcode.com/problems/maximum-profit-in-job-scheduling/discuss/409009/JavaC%2B%2BPython-DP-Solution
+
+        int const startTimeSize = startTime.size();
+        int const endTimeSize = endTime.size();
+        int const profitSize = profit.size();
+
+        if (JUST_RETURN_ZERO == startTimeSize) {
+            return 0;
+        }
+        if (!(startTimeSize == endTimeSize && startTimeSize == profitSize)) {
+            return -1;
+        }
+        if (JUST_RETURN_ONE == startTimeSize) {
+            return profit.back();
+        }
+        std::vector<std::tuple<int, int, int>> jobs;
+        for (int idx = 0; idx < endTimeSize; idx++) {
+            jobs.emplace_back(std::make_tuple(endTime[idx], startTime[idx], profit[idx]));
+        }
+        std::sort(jobs.begin(), jobs.end());
+
+        std::map<int, int> dpMemo{{0, 0}};
+        for (auto const & job : jobs) {
+            /* // job can't be reference if using tuple_size
+            if (TUPLE_PROFIX >= std::tuple_size<decltype(job)>::value) {
+                continue;
+            }
+            */
+            int curProfit = std::prev(dpMemo.upper_bound(std::get<TUPLE_START_TIME>(job)))->second +
+                std::get<TUPLE_PROFIX>(job);
+            if (curProfit > dpMemo.rbegin()->second) {
+                dpMemo[std::get<TUPLE_END_TIME>(job)] = curProfit;
+            }
+        }
+
+        return dpMemo.rbegin()->second;
+    }
+private:
+    static int const JUST_RETURN_ZERO = 0;
+    static int const JUST_RETURN_ONE = 1;
+    static int const TUPLE_END_TIME = 0;
+    static int const TUPLE_START_TIME = 1;
+    static int const TUPLE_PROFIX = 2;
+};
