49b0ef176babe5904e748f2abb4d04f7f9d823db
gaoxiaoxing <gaoxiaoxing@baidu.com>
2021-01-09 10:48:29 +0800

    add design_a_stack_with_increment_operation_1381 some other's solutioin to implement statck by point

--- src/main/algorithms/cpp/stack/design_a_stack_with_increment_operation_1381/solution.cpp
+++ src/main/algorithms/cpp/stack/design_a_stack_with_increment_operation_1381/solution.cpp
@@ -44,3 +44,45 @@ private:
     int const _cStackSize;
     std::vector<int> _cStack;
 };
+
+const static int _ = []() {
+	cin.tie(0);
+	cout.tie(0);
+	ios_base::sync_with_stdio(false);
+	return 0;
+}();
+
+class CustomStack {
+public:
+
+    int *stack,*lazy;
+    int ptr, maxSize;
+
+    CustomStack(int maxSize):ptr(0),maxSize(maxSize) {
+        stack = (int*)calloc((maxSize+7),sizeof(int));
+        lazy = (int*)calloc((maxSize+7),sizeof(int));
+        //memset(lazy,0,1007*sizeof(int));
+        //memset(stack,0,1007*sizeof(int));
+    }
+    inline void push(int x) {
+        if(ptr+1>maxSize) return;
+        stack[ptr++]=x;
+    }
+
+    inline int pop() {
+        if(ptr==0) return -1;
+        int x = stack[ptr-1]+lazy[ptr-1];
+        if(ptr-2>=0) {
+            lazy[ptr-2]+=lazy[ptr-1];
+            lazy[ptr-1]=0;
+        } else {
+            lazy[0]=0;
+        }
+        ptr--;
+        return x;
+    }
+    inline
+    void increment(int k, int val) {
+        if(ptr>0) lazy[min(ptr-1,k-1)]+=val;
+    }
+};
