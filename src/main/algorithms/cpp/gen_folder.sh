a608e6bca42619c448d9709c90a40186c33d7e32
gaoxiaoxing <gaoxiaoxing@baidu.com>
2021-01-12 15:09:26 +0800

    add a default argument for gen_folder.sh. which means we can just execute shell 'sh gen_folder.sh' and it will read the res file in the local folder

--- src/main/algorithms/cpp/gen_folder.sh
+++ src/main/algorithms/cpp/gen_folder.sh
@@ -1,5 +1,6 @@
+inputFile=${1:-"res"}
 # dirName=$(sed 's/ /_/g' $1 | tr '[A-Z]' '[a-z]')
-dirName=$(sed  's/\(.*\)\(\.\) \(.*\)/\3 \1/g' $1 | sed 's/[ -]/_/g' | sed 's/[^[:alnum:]_]/_/g' | tr '[A-Z]' '[a-z]')
+dirName=$(sed  's/\(.*\)\(\.\) \(.*\)/\3 \1/g' "${inputFile}" | sed 's/[ -]/_/g' | sed 's/[^[:alnum:]_]/_/g' | tr '[A-Z]' '[a-z]')
 mkdir "./${dirName}" &&\
 cd "./${dirName}" &&\
 touch solution.cpp && touch description.txt && touch core.solution &&\
