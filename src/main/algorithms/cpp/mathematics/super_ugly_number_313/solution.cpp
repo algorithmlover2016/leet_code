15a162be0686a868441a06d1861cbffad0d57128
gaoxiaoxing <gaoxiaoxing@baidu.com>
2021-02-26 12:31:06 +0800

    modify plagiarized solution to easily understand for super_ugly_number_313

--- src/main/algorithms/cpp/mathematics/super_ugly_number_313/solution.cpp
+++ src/main/algorithms/cpp/mathematics/super_ugly_number_313/solution.cpp
@@ -45,3 +45,52 @@ public:
         return minHeap.top();
     }
 };
+
+#define EACH_PRIME_COMPUTE_MIN
+// #define DEBUG
+class Solution {
+public:
+    int nthSuperUglyNumber(int n, std::vector<int> const & primes) {
+        if (n == 1) {
+            return 1;
+        }
+
+        std::vector<int> dp(n + 1, INT_MAX);
+        dp[1] = 1;
+
+        int m = primes.size();
+        // record the primes[i] using times;
+        std::vector<int> indexj(m, 1);
+        // record the next used element
+        std::vector<int> nexts(m, 1);
+
+        for (int i=2; i<=n; i++) {
+            for (int p=0; p<m; p++) {
+                #ifdef DEBUG
+                std::cout << p << ",\t" << dp[i - 1] << ",\t" << indexj[p] << ",\t" << nexts[p] << "\t";
+                #endif
+                if (nexts[p] == dp[i-1]) {
+                    // nexts[p] need update, it should become bigger.
+                    // all the elements come from nexts
+                    // the last element that use primes[p] is indexj[p]
+                    // or the primes[p] create indexj[p] elements, and it need create a new one
+
+                    nexts[p] = dp[indexj[p]] * primes[p];
+                    indexj[p]++;
+                }
+                #ifdef DEBUG
+                std::cout << "\n";
+                #endif
+#ifdef EACH_PRIME_COMPUTE_MIN
+                dp[i] = std::min(dp[i], nexts[p]);
+#endif
+            }
+
+#ifndef EACH_PRIME_COMPUTE_MIN
+            dp[i] = *std::min_element(nexts.begin(), nexts.end());
+#endif
+        }
+
+        return dp[n];
+    }
+};
