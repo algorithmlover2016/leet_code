fbe409a2254ff8ca32f9f50f5250c5820b87783a
gaoxiaoxing <gaoxiaoxing@baidu.com>
2021-02-28 13:54:08 +0800

    add some explanation for copy_list_with_random_pointer_138 first insert a node after the original node, and then change the random point, finally split into oldlist and newlist

--- src/main/algorithms/cpp/list/copy_list_with_random_pointer_138/solution.cpp
+++ src/main/algorithms/cpp/list/copy_list_with_random_pointer_138/solution.cpp
@@ -41,6 +41,7 @@ public:
 
         while (current != nullptr) {
             temp = current->next;
+            // add a new deep copy node and its random is nullptr
             current->next = new Node(current->val, nullptr, nullptr);
             current->next->next = temp;
             current = temp;
@@ -49,9 +50,11 @@ public:
         current = head;
 
         while (current) {
-            if(current->next && current->random) {
+            if (current->next && current->random) {
+                /*it's not a copy node and it's random not be empty*/
                 current->next->random = current->random->next;
             }
+            // each time take two step, unless untill to end end(nullptr);
             current = current->next ? current->next->next : current->next;
         }
 
@@ -59,13 +62,14 @@ public:
         Node* new_node = head->next;
         Node* new_head = head->next;
 
-        while(old_node != nullptr && new_node != nullptr) {
+        while (old_node != nullptr && new_node != nullptr) {
             old_node->next = old_node->next != nullptr ? old_node->next->next : old_node->next;
             new_node->next = new_node->next != nullptr ? new_node->next->next : new_node->next;
 
             old_node = old_node->next;
             new_node = new_node->next;
         }
+
         return new_head;
     }
 };
