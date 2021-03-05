1e8dba06ea6093b11b97ddc436de8d17ba9843e0
gaoxiaoxing <gaoxiaoxing@baidu.com>
2021-03-05 14:19:29 +0800

    add some namespace for find_the_closest_palindrome_564

--- src/main/algorithms/cpp/string/find_the_closest_palindrome_564/solution.cpp
+++ src/main/algorithms/cpp/string/find_the_closest_palindrome_564/solution.cpp
@@ -2,23 +2,26 @@
 
 class SolutionPlagiarizeOther {
 public:
-    string nearestPalindromic(string n) {
+    std::string nearestPalindromic(std::string const & n) {
         int l = n.size() / 2;
-        string left = n.substr(0, l);
-        string mid = n.substr(l, n.size() - 2 * l);
-        int ln = left.empty() ? 0 : stoi(left);
-        long num = stol(n);
-        long n3 = stol(left + mid + string(left.rbegin(), left.rend()));
+        std::string left = n.substr(0, l);
+        std::string mid = n.substr(l, n.size() - 2 * l);
+        int ln = left.empty() ? 0 : std::stoi(left);
+        long num = std::stol(n);
+        long n3 = std::stol(left + mid + std::string(left.rbegin(), left.rend()));
         long lo, hi;
-        if (n3 < num) lo = n3;
-        else {
-            string left1 = left, mid1 = mid;
+        if (n3 < num) {
+            lo = n3;
+        } else {
+            std::string left1 = left, mid1 = mid;
             if (n.size() % 2 == 0) {
+                // mid = ""
                 int ln1 = ln - 1;
-                left1 = ln1 == 0 ? "" : to_string(ln1);
-                if (left1.size() < l) mid1 = "9";
-            }
-            else if (mid1 == "0") {
+                left1 = ln1 == 0 ? "" : std::to_string(ln1);
+                if (left1.size() < l) {
+                    mid1 = "9";
+                }
+            } else if (mid1 == "0") {
                 int ln1 = ln - 1;
                 left1 = ln1 == 0 ? "" : to_string(ln1);
                 if (left1.size() == l) mid1 = "9";
@@ -26,31 +29,37 @@ public:
                     left1 += '9';
                     mid1 = "";
                 }
+            } else {
+                mid1 = std::to_string(mid1[0] - '0' - 1);
             }
-            else mid1 = to_string(mid1[0] - '0' - 1);
-            lo = stol(left1 + mid1 + string(left1.rbegin(), left1.rend()));
+            lo = std::stol(left1 + mid1 + std::string(left1.rbegin(), left1.rend()));
         }
-        if (n3 > num) hi = n3;
-        else {
-            string left2 = left, mid2 = mid;
+        if (n3 > num) {
+            hi = n3;
+        } else {
+            std::string left2 = left, mid2 = mid;
             if (n.size() % 2 == 0) {
+                // mid = "";
                 int ln2 = ln + 1;
-                left2 = to_string(ln2);
+                left2 = std::to_string(ln2);
                 if (left2.size() > l) {
                     left2 = left2.substr(0, l);
                     mid2 = "0";
                 }
-            }
-            else if (mid2 == "9") {
+            } else if (mid2 == "9") {
                 int ln2 = ln + 1;
-                left2 = to_string(ln2);
-                if (left2.size() == l) mid2 = "0";
-                else mid2 = "";
+                left2 = std::to_string(ln2);
+                if (left2.size() == l) {
+                    mid2 = "0";
+                } else {
+                    mid2 = "";
+                }
+            } else {
+                mid2 = std::to_string(mid2[0] - '0' + 1);
             }
-            else mid2 = to_string(mid2[0] - '0' + 1);
-            hi = stol(left2 + mid2 + string(left2.rbegin(), left2.rend()));
+            hi = std::stol(left2 + mid2 + std::string(left2.rbegin(), left2.rend()));
         }
-        return num - lo > hi - num ? to_string(hi) : to_string(lo);
+        return num - lo > hi - num ? std::to_string(hi) : std::to_string(lo);
     }
 };
 
@@ -74,7 +83,7 @@ public:
         std::string lower = normalize(std::to_string(lowerNum));
         long long int upperDiff = std::stoll(upper) - nNum;
         long long int lowerDiff = nNum - std::stoll(lower);
-        long long int normalDiff = abs(std::stoll(normal) - nNum);
+        long long int normalDiff = std::abs(std::stoll(normal) - nNum);
         std::string ans = lower;
         // std::cout << "normal: " << normal << " diff: " << normalDiff << "\n"
         //     << "upper: " << upper << " diff: " << upperDiff << "\n"
