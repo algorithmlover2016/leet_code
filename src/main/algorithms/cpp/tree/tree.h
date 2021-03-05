d2ae8aea29f7b500b6cb20d3264339f5074def7a
gaoxiaoxing <gaoxiaoxing@baidu.com>
2021-03-05 14:19:57 +0800

    add a PTreeNode struct which has parent pointer

--- src/main/algorithms/cpp/tree/tree.h
+++ src/main/algorithms/cpp/tree/tree.h
@@ -11,4 +11,15 @@
      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  };
+
+ struct PTreeNode {
+     int val;
+     PTreeNode *left;
+     PTreeNode *right;
+     PTreeNode *parent;
+     PTreeNode() : val(0), left(nullptr), right(nullptr), parent(nullptr) {}
+     PTreeNode(int x) : val(x), left(nullptr), right(nullptr), parent(nullptr) {}
+     PTreeNode(int x, PTreeNode *left, PTreeNode *right, PTreeNode* parent_)
+         : val(x), left(left), right(right), parent(parent_) {}
+ };
 #endif
