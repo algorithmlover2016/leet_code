d7ad40cc6c51349a9552cf34f409f56f5bb0305d
alogirhtmlover <yubo.upt@gmail.com>
2021-12-06 17:58:43 +0800

    add Fenwick Tree solution for find_the_longest_valid_obstacle_course_at_each_position_1964

--- src/main/algorithms/cpp/array/find_the_longest_valid_obstacle_course_at_each_position_1964/core.md
+++ src/main/algorithms/cpp/array/find_the_longest_valid_obstacle_course_at_each_position_1964/core.md
@@ -1,7 +1,7 @@
-# longest increasing series(LIS)
+# longest increasing sequence(LIS)
 * **strictly increasing**<br>
     ***`std::lower_bound`***<br>
-* ****<br>
+* **non-decreaing**<br>
     ***`std::upper_bound`***<br>
 ## demo code
 ```
@@ -20,3 +20,26 @@ for (int idx = 0; idx < nums.size(); idx++) {
     }
 }
 ```
+
+# Fenwick Tree
+* **get the value by `idx` to avoid too many search by `idx -= (idx & (-idx))`, however, `idx` must be geater than zero**<br>
+```
+int get(int idx) {
+    int ans = 0;
+    for (; idx > 0; idx -= idx & (-idx)) {
+        // user-defined logic for the problem
+        ans = std::max(ans, bits[idx]);
+    }
+    return ans;
+}
+```
+* **update the value by update from `idx` to the end by `idx += (idx & (-idx))`**<br>
+```
+void update(int idx, int val) {
+    for (; idx < bits.size(); idx += idx & (-idx)) {
+        // user-defined logic for the problem
+        bits[idx] = std::max(bits[idx], val);
+    }
+}
+```
+* **the reason in this problem why use max function is because we need consider the order of elements appearing. And all the former elements can be seen by the later**<br>
--- src/main/algorithms/cpp/array/find_the_longest_valid_obstacle_course_at_each_position_1964/solution.cpp
+++ src/main/algorithms/cpp/array/find_the_longest_valid_obstacle_course_at_each_position_1964/solution.cpp
@@ -41,3 +41,56 @@ public:
         return ans;
     }
 };
+
+class SolutionFenwickTree {
+private:
+    class MaxBitByFenwickTree {
+    private:
+        std::vector<int> bits;
+    public:
+        MaxBitByFenwickTree(int size) {
+            bits.resize(size + 1);
+        }
+        int get(int idx) {
+            int ans = 0;
+            for (; idx > 0; idx -= idx & (-idx)) {
+                ans = std::max(ans, bits[idx]);
+            }
+            return ans;
+        }
+        void update(int idx, int val) {
+            for (; idx < bits.size(); idx += idx & (-idx)) {
+                bits[idx] = std::max(bits[idx], val);
+            }
+        }
+
+    };
+public:
+    std::vector<int> longestObstacleCourseAtEachPosition(std::vector<int> const & obstacles) {
+        // plagiarizing from https://leetcode.com/problems/find-the-longest-valid-obstacle-course-at-each-position/discuss/1390159/C%2B%2BPython-Same-with-Longest-Increasing-Subsequence-problem-Clean-and-Concise
+        // Fenwick Tree with Compression
+        std::vector<int> obstaclesIdx(obstacles);
+        std::vector<int> ans(obstacles.size());
+        int uniqCompressSize = compress(obstaclesIdx);
+        MaxBitByFenwickTree maxBitObj(uniqCompressSize);
+        for (int idx = 0; idx < obstaclesIdx.size(); idx++) {
+            int order = obstaclesIdx[idx];
+            int subLongest = maxBitObj.get(order) + 1;
+            ans[idx] = subLongest;
+            maxBitObj.update(order, subLongest);
+        }
+        return ans;
+    }
+
+private:
+    int compress(std::vector<int> & obstaclesIdx) {
+        std::vector<int> strictlyIncObstacles(obstaclesIdx);
+        std::sort(std::begin(strictlyIncObstacles), std::end(strictlyIncObstacles));
+        strictlyIncObstacles.erase(std::unique(strictlyIncObstacles.begin(), strictlyIncObstacles.end()), strictlyIncObstacles.end());
+        // change val from value of obstacles into index order after sorted increasing
+        for (int & val : obstaclesIdx) {
+            val = std::lower_bound(std::begin(strictlyIncObstacles), std::end(strictlyIncObstacles), val) - std::begin(strictlyIncObstacles) + 1;
+        }
+        return strictlyIncObstacles.size();
+    }
+};
