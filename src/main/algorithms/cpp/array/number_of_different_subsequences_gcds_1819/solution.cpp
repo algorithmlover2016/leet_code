a39204cf56554e5fd26aca1082dd5204587ebf3f
alogirhtmlover <yubo.upt@gmail.com>
2022-01-31 12:46:20 +0800

    add discription for number_of_different_subsequences_gcds_1819

---
+++ src/main/algorithms/cpp/array/number_of_different_subsequences_gcds_1819/solution.cpp
@@ -0,0 +1,10 @@
+#include "../../head.h"
+
+
+
+class Solution {
+public:
+    int countDifferentSubsequenceGCDs(std::vector<int> const & nums) {
+        // plagiarizing from https://leetcode.com/problems/number-of-different-subsequences-gcds/discuss/1141309/Simple-explanation-or-O(n-*-sqrt-n)
+    }
+};
