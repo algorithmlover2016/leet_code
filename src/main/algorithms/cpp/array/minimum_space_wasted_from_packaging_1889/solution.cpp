18982df63b8ac9c155bb14ef5f8d3a7ee7eb673c
alogirhtmlover <yubo.upt@gmail.com>
2022-05-19 14:22:33 +0800

    add description for minimum_space_wasted_from_packaging_1889

---
+++ src/main/algorithms/cpp/array/minimum_space_wasted_from_packaging_1889/solution.cpp
@@ -0,0 +1,12 @@
+#include "../../head.h"
+
+
+
+class Solution {
+public:
+    int minWastedSpace(std::vector<int> const & packages, std::vector<std::vector<int>> const & boxes) {
+        
+    }
+private:
+    int const MOD = 1e9 + 7;
+};
