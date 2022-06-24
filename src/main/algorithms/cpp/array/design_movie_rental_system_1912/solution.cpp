c6fb6be77dcdffcdd0d317eb425ba4ba3e2a2aae
alogirhtmlover <yubo.upt@gmail.com>
2022-06-24 14:22:49 +0800

    add description for design_movie_rental_system_1912

---
+++ src/main/algorithms/cpp/array/design_movie_rental_system_1912/solution.cpp
@@ -0,0 +1,35 @@
+#include "../../head.h"
+
+
+
+class MovieRentingSystem {
+public:
+    MovieRentingSystem(int n, std::vector<std::vector<int>>& entries) {
+          
+    }
+    
+    std::vector<int> search(int movie) {
+        
+    }
+    
+    void rent(int shop, int movie) {
+        
+    }
+    
+    void drop(int shop, int movie) {
+        
+    }
+    
+    std::vector<std::vector<int>> report() {
+        
+    }
+};
+
+/**
+ * Your MovieRentingSystem object will be instantiated and called as such:
+ * MovieRentingSystem* obj = new MovieRentingSystem(n, entries);
+ * vector<int> param_1 = obj->search(movie);
+ * obj->rent(shop,movie);
+ * obj->drop(shop,movie);
+ * vector<vector<int>> param_4 = obj->report();
+ */
