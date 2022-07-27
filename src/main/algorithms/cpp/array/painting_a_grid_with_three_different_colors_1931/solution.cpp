ed301949dabea9e9bf3513ec52e4f08552c13821
alogirhtmlover <yubo.upt@gmail.com>
2022-07-27 17:50:00 +0800

    add unfinished codes for painting_a_grid_with_three_different_colors_1931

--- src/main/algorithms/cpp/array/painting_a_grid_with_three_different_colors_1931/solution.cpp
+++ src/main/algorithms/cpp/array/painting_a_grid_with_three_different_colors_1931/solution.cpp
@@ -4,6 +4,19 @@
 class Solution {
 public:
     int colorTheGrid(int m, int n) {
-        
+        this->row = m;
+        this->col = n;
+        this->max_mask = (COLOR_BASE << m);
+        memo = std::vector<std::vector<int>>(n, std::vector<int>(max_mask));
+        curCol = std::vector(max_mask, std::vector<int>());
     }
+private:
+    static int const MAX_COL = 1000;
+    static int const COLOR_BITS = 2;
+    static int const COLOR_BASE = (2 << COLOR_BITS);
+    int row;
+    int col;
+    int max_mask;
+    std::vector<std::vector<int>> memo;
+    std::vector<std::vector<int>> curCol;
 };
