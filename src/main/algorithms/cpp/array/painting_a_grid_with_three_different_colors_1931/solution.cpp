eeb47d5c3847540446e70cf9c645ea3d78be7e71
alogirhtmlover <yubo.upt@gmail.com>
2022-07-29 14:53:26 +0800

    add solution with one function for painting_a_grid_with_three_different_colors_1931

--- src/main/algorithms/cpp/array/painting_a_grid_with_three_different_colors_1931/solution.cpp
+++ src/main/algorithms/cpp/array/painting_a_grid_with_three_different_colors_1931/solution.cpp
@@ -3,7 +3,7 @@
 
 // #define DEBUG
 
-class Solution {
+class SolutionDPDFS {
 public:
     int colorTheGrid(int m, int n) {
         // plagiarizing from https://leetcode.com/problems/painting-a-grid-with-three-different-colors/discuss/1330185/C%2B%2BPython-DP-and-DFS-and-Bitmask-Picture-explain-Clean-and-Concise
@@ -76,4 +76,50 @@ private:
     int max_mask;
     std::vector<std::vector<int>> memo;
     std::vector<std::vector<int>> curCol;
+};
+
+class Solution {
+public:
+    int colorTheGrid(int const m, int const n, int row = 0, int col = 0, int cur = 0, int pre = 0) {
+
+        if (0 == (row | col)) {
+            dpMemo = std::vector(MAX_COL, std::vector<int>((1 << (COLOR_BITS * m)), INIT_VAL));
+        }
+
+        // plagiarizing from https://leetcode.com/problems/painting-a-grid-with-three-different-colors/discuss/1330174/Top-down-DP-with-bit-mask
+        if (m == row) {
+            // it means we set all bits in this col
+            return colorTheGrid(m, n, 0, col + 1, 0, cur);
+        }
+        if (n == col) {
+            // we find a combination
+            return 1;
+        }
+
+        if (0 == row && INIT_VAL != dpMemo[col][pre] ) {
+            return dpMemo[col][pre];
+        }
+
+        int upper = (0 == row ? INIT_VAL : ((cur >> (COLOR_BITS * (row - 1))) & (COLOR_BASE - 1)));
+        int left = (pre >> (COLOR_BITS * row)) & (COLOR_BASE - 1);
+        int ans = 0;
+        for (int color = INIT_VAL + 1; color <COLOR_BASE; color++) {
+            if (color != upper && color != left) {
+                ans = (ans + colorTheGrid(m, n, row + 1, col, cur | (color << (COLOR_BITS * row)), pre)) % MOD;
+            }
+        }
+        if (0 == row) {
+            dpMemo[col][pre] = ans;
+        }
+        return ans;
+
+    }
+private:
+    std::vector<std::vector<int>> dpMemo;
+private:
+    static int const MAX_COL = 1000;
+    static int const COLOR_BITS = 2;
+    static int const COLOR_BASE = (1 << COLOR_BITS);
+    static int const MOD = 1e9 + 7;
+    int const INIT_VAL = 0;
 };
