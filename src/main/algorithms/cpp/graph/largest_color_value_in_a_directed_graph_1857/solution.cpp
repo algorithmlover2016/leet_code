1c2672ef7bc5f63f5adea416ff05edeb424c5335
alogirhtmlover <yubo.upt@gmail.com>
2022-08-02 16:10:09 +0800

    add dfs solution for largest_color_value_in_a_directed_graph_1857

--- src/main/algorithms/cpp/graph/largest_color_value_in_a_directed_graph_1857/solution.cpp
+++ src/main/algorithms/cpp/graph/largest_color_value_in_a_directed_graph_1857/solution.cpp
@@ -2,7 +2,7 @@
 
 // #define DEBUG
 #define ADD_WHEN_CNT
-class Solution {
+class SolutionTopologicalSort {
 public:
     int largestPathValue(std::string const & colors, std::vector<std::vector<int>> const & edges) {
         // plagiarizing from https://leetcode.com/problems/largest-color-value-in-a-directed-graph/discuss/1198658/C%2B%2B-Topological-Sort
@@ -67,6 +67,59 @@ public:
     }
 private:
     static int const LOWERCASE_CNT = 26;
+    static int const EDGE_LEN = 2;
+    static int const EDGE_START_IDX = 0;
+    static int const EDGE_END_IDX = 1;
+    static char const offset = 'a';
+};
+
+class SolutionDFS {
+public:
+    int largestPathValue(std::string const & colors, std::vector<std::vector<int>> const & edges) {
+        // plagiarizing from https://leetcode.com/problems/largest-color-value-in-a-directed-graph/discuss/1200575/Topological-Sort-vs.-DFS-vs.-Dijkstra
+        int nodesCnt = colors.size();
+        // std::vector<std::array<int, LOWERCASE_CNT>> curNodesColorsCnt(nodesCnt, std::array<int, LOWERCASE_CNT>({{0}}));
+        std::vector<std::array<int, LOWERCASE_CNT>> curNodesColorsCnt(nodesCnt);
+
+        std::vector<std::vector<int>> directedGraph(nodesCnt);
+        for (auto const & edge : edges) {
+            if (edge.size() < EDGE_LEN) {
+                continue;
+            }
+            directedGraph[edge[EDGE_START_IDX]].emplace_back(edge[EDGE_END_IDX]);
+        }
+
+        std::vector<int> visited(nodesCnt);
+        int ans = 0;
+        for (int idx = 0; idx < nodesCnt && ans != INT_MAX; idx++) {
+            ans = std::max(ans, dfs(idx, directedGraph, colors, curNodesColorsCnt, visited));
+        }
+        return ans == INT_MAX ? -1 : ans;
+    }
+private:
+    static int const LOWERCASE_CNT = 26;
+    int dfs(int curNode,
+            std::vector<std::vector<int>> const & directedGraph,
+            std::string const & colors,
+            std::vector<std::array<int, LOWERCASE_CNT>> & curNodesColorsCnt,
+            std::vector<int> & visited) {
+        if (!visited[curNode]) {
+            visited[curNode] = 1;
+            for (int next : directedGraph[curNode]) {
+                if (dfs(next, directedGraph, colors, curNodesColorsCnt, visited) == INT_MAX) {
+                    return INT_MAX;
+                }
+                for (int idx = 0; idx < LOWERCASE_CNT; idx++) {
+                    curNodesColorsCnt[curNode][idx] = std::max(curNodesColorsCnt[curNode][idx], curNodesColorsCnt[next][idx]);
+                }
+            }
+            curNodesColorsCnt[curNode][colors[curNode] - offset]++;
+            visited[curNode] = 2;
+        }
+        return visited[curNode] == 2 ?  curNodesColorsCnt[curNode][colors[curNode] - offset] : INT_MAX;
+    }
+
+private:
     static int const EDGE_LEN = 2;
     static int const EDGE_START_IDX = 0;
     static int const EDGE_END_IDX = 1;
