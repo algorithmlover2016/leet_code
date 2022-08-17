7d02c2df752e6a3d79cf50b2c51b9f5a799a4b8c
alogirhtmlover <yubo.upt@gmail.com>
2022-08-17 15:48:43 +0800

    add solution for shortest_impossible_sequence_of_rolls_2350

--- src/main/algorithms/cpp/array/shortest_impossible_sequence_of_rolls_2350/solution.cpp
+++ src/main/algorithms/cpp/array/shortest_impossible_sequence_of_rolls_2350/solution.cpp
@@ -4,6 +4,20 @@
 class Solution {
 public:
     int shortestSequence(std::vector<int> const & rolls, int k) {
+        // Find a shortest subsequence that can not be taken from rolls.
+        // We can remove some roll but can not change the order.
+        // plagiarizing from https://leetcode.com/problems/shortest-impossible-sequence-of-rolls/discuss/2322280/JavaC%2B%2BPython-One-Pass-O(K)-Space
+        std::set<int> visited;
+        int ans = 1;
+        for (int const roll : rolls) {
+            visited.insert(roll);
+            if (visited.size() == k) {
+                // it means we find a circle of all the numbers from 1 to k
+                ans++; // after increament 1, ans will be the next length that need find a circle.
+                visited.clear();
+            }
+        }
+        return ans;
         
     }
 };
