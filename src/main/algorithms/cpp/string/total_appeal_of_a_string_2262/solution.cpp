02c0e7b9b9d693aeb6639bb00adfe7c0a9cc6a50
alogirhtmlover <yubo.upt@gmail.com>
2022-08-30 09:53:32 +0800

    add dynamic solution for total_appeal_of_a_string_2262

--- src/main/algorithms/cpp/string/total_appeal_of_a_string_2262/solution.cpp
+++ src/main/algorithms/cpp/string/total_appeal_of_a_string_2262/solution.cpp
@@ -4,6 +4,19 @@
 class Solution {
 public:
     long long appealSum(std::string const & s) {
+        // plagiarizing from https://leetcode.com/problems/total-appeal-of-a-string/discuss/1996203/DP
+        long long ans = 0, cur = 0;
+        std::vector<int> prev(CHAR_LEN, -1);
+        for (int idx = 0; idx < s.size(); idx++) {
+            int cIdx = s[idx] - OFFSET;
+            cur += idx - prev[cIdx]; // compared with the (idx - 1)th ele, we only add one for each substr from the prev[cIdx] + 1（include) to idx
+            prev[cIdx] = idx;
+            ans += cur;
+        }
+        return ans;
         
     }
+private:
+    static char const OFFSET = 'a';
+    static int const CHAR_LEN = 26;
 };
