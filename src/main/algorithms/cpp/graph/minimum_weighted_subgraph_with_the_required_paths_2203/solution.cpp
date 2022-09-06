820260cfd04d7c8a34293182cb8c8c73810d5811
alogirhtmlover <yubo.upt@gmail.com>
2022-09-06 15:35:09 +0800

    add description for minimum_weighted_subgraph_with_the_required_paths_2203

---
+++ src/main/algorithms/cpp/graph/minimum_weighted_subgraph_with_the_required_paths_2203/solution.cpp
@@ -0,0 +1,10 @@
+#include "../../head.h"
+
+
+
+
+class Solution {
+public:
+    long long minimumWeight(int n, std::vector<std::vector<int>> const & edges, int src1, int src2, int dest) {
+    }
+};
