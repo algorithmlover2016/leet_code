75f42b39fc805212ebcf2a7a8f9c63b5bdd98ecf
alogirhtmlover <yubo.upt@gmail.com>
2022-09-19 10:42:51 +0800

    add description for design_a_text_editor_2296

---
+++ src/main/algorithms/cpp/string/design_a_text_editor_2296/solution.cpp
@@ -0,0 +1,33 @@
+#include "../../head.h"
+
+/**
+ * Your TextEditor object will be instantiated and called as such:
+ * TextEditor* obj = new TextEditor();
+ * obj->addText(text);
+ * int param_2 = obj->deleteText(k);
+ * string param_3 = obj->cursorLeft(k);
+ * string param_4 = obj->cursorRight(k);
+ */
+
+class TextEditor {
+public:
+    TextEditor() {
+        
+    }
+    
+    void addText(std::string const & text) {
+        
+    }
+    
+    int deleteText(int k) {
+        
+    }
+    
+    std::string cursorLeft(int k) {
+        
+    }
+    
+    std::string cursorRight(int k) {
+        
+    }
+};
