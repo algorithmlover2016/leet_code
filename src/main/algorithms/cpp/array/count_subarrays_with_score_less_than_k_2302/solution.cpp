a511aadfa968dbe4ebc2e9c01436ce475c4358a1
alogirhtmlover <yubo.upt@gmail.com>
2022-09-26 09:16:21 +0800

    add brute force solution for count_subarrays_with_score_less_than_k_2302

--- src/main/algorithms/cpp/array/count_subarrays_with_score_less_than_k_2302/solution.cpp
+++ src/main/algorithms/cpp/array/count_subarrays_with_score_less_than_k_2302/solution.cpp
@@ -1,9 +1,25 @@
 #include "../../head.h"
 
 
-class Solution {
+class SolutionTLE {
 public:
     long long countSubarrays(std::vector<int> const & nums, long long k) {
-        
+        if (nums.empty()) {
+            return 0;
+        }
+        std::vector<int> preSums(nums.size() + 1, 0);
+        for (int idx = 0; idx < nums.size(); idx++) {
+            preSums[idx + 1] = preSums[idx] + nums[idx];
+        }
+        long long ans = 0;
+        for (int len = 1; len <= nums.size(); len++) {
+            for (int idx = len; idx <= nums.size(); idx++) {
+                long long mul = (long long) (preSums[idx] - preSums[idx - len]) * len;
+                if (mul < k) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
     }
 };
