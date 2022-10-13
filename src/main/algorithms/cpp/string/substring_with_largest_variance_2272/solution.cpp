919c1f555cb8a53046db7902d4667d2c6df6c5e1
alogirhtmlover <yubo.upt@gmail.com>
2022-10-13 16:35:32 +0800

    add description for substring_with_largest_variance_2272

--- src/main/algorithms/cpp/string/substring_with_largest_variance_2272/solution.cpp
+++ src/main/algorithms/cpp/string/substring_with_largest_variance_2272/solution.cpp
@@ -1,4 +1,13 @@
--e #include "../../head.h"
+#include "../../head.h"
 
 
-
+class Solution {
+public:
+    int largestVariance(std::string const & s) {
+        if (s.size() < 2) {
+            return 0;
+        }
+        return 0;
+    }
+private:
+};
