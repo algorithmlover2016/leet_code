8ade2c983ab195fd455d91027f13cd6c856ebad0
alogirhtmlover <yubo.upt@gmail.com>
2022-11-03 14:25:30 +0800

    add description for create_components_with_same_value_2440

---
+++ src/main/algorithms/cpp/graph/create_components_with_same_value_2440/solution.cpp
@@ -0,0 +1,9 @@
+#include "../../head.h"
+
+
+class Solution {
+public:
+    int componentValue(std::vector<int> const & nums, std::vector<std::vector<int>> const & edges) {
+        return 0;
+    }
+};
